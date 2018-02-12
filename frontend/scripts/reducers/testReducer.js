a2768cee2f4415e83a434b0b56a8feffabd516f3
Adrian <adrianjewell91@gmail.com>
2018-02-12 09:15:43 -0800

    init

---
+++ frontend/scripts/reducers/testReducer.js
@@ -0,0 +1,9 @@
+export const testReducer = (state = [], action) => {
+    Object.freeze(state);
+    // switch (action.type) {
+    //   case RECEIVE_PRODUCTS:
+    //     return action.products;
+    //   default:
+        return state;
+    // }
+  };
